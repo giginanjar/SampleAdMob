439aa8bf96a2121f6e181298ed1cd66cba460612
giginanjar <gynz.yudhis32@gmail.com>
2016-05-31 11:33:36 +0700

    Initial commit

---
+++ app/src/main/java/com/gegemobile/sampleadmob/MainActivity.java
@@ -0,0 +1,270 @@
+package com.gegemobile.sampleadmob;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.NativeExpressAdView;
+import com.google.android.gms.ads.formats.NativeAppInstallAd;
+import com.google.android.gms.ads.formats.NativeAppInstallAdView;
+import com.google.android.gms.ads.formats.NativeContentAd;
+import com.google.android.gms.ads.formats.NativeContentAdView;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String ADMOB_AD_UNIT_ID = "ca-app-pub-1270633169776298/1702081963";
+    private static final String ADMOB_APP_ID = "ca-app-pub-1270633169776298~7748615565";
+    static final int REQUEST_CODE_RECOVER_PLAY_SERVICES = 1001;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private Button mRefresh;
+    private CheckBox mRequestAppInstallAds;
+    private CheckBox mRequestContentAds;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        NativeExpressAdView adView = (NativeExpressAdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+//                .addTestDevice("1752DF90D3C9D717D40EA68DBF1976BC")
+                .build();
+        adView.loadAd(adRequest);
+        /*MobileAds.initialize(this, ADMOB_APP_ID);
+
+        mRefresh = (Button) findViewById(R.id.btn_refresh);
+        mRequestAppInstallAds = (CheckBox) findViewById(R.id.cb_appinstall);
+        mRequestContentAds = (CheckBox) findViewById(R.id.cb_content);
+
+        mRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshAd(mRequestAppInstallAds.isChecked(),
+                        mRequestContentAds.isChecked());
+            }
+        });
+
+        refreshAd(mRequestAppInstallAds.isChecked(),
+                mRequestContentAds.isChecked());*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkGooglePlayServices();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean checkGooglePlayServices(){
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if(resultCode != ConnectionResult.SUCCESS){
+            if(apiAvailability.isUserResolvableError(resultCode)){
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            }else {
+                Toast.makeText(this, "device_not_supported",
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+    /*
+    *//**
+     * Populates a {@link NativeAppInstallAdView} object with data from a given
+     * {@link NativeAppInstallAd}.
+     *
+     * @param nativeAppInstallAd the object containing the ad's assets
+     * @param adView             the view to be populated
+     *//*
+    private void populateAppInstallAdView(NativeAppInstallAd nativeAppInstallAd,
+                                          NativeAppInstallAdView adView) {
+        adView.setHeadlineView(adView.findViewById(R.id.appinstall_headline));
+        adView.setImageView(adView.findViewById(R.id.appinstall_image));
+        adView.setBodyView(adView.findViewById(R.id.appinstall_body));
+        adView.setCallToActionView(adView.findViewById(R.id.appinstall_call_to_action));
+        adView.setIconView(adView.findViewById(R.id.appinstall_app_icon));
+        adView.setPriceView(adView.findViewById(R.id.appinstall_price));
+        adView.setStarRatingView(adView.findViewById(R.id.appinstall_stars));
+        adView.setStoreView(adView.findViewById(R.id.appinstall_store));
+
+        // Some assets are guaranteed to be in every NativeAppInstallAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeAppInstallAd.getHeadline());
+        ((TextView) adView.getBodyView()).setText(nativeAppInstallAd.getBody());
+        ((Button) adView.getCallToActionView()).setText(nativeAppInstallAd.getCallToAction());
+        ((ImageView) adView.getIconView()).setImageDrawable(nativeAppInstallAd.getIcon()
+                .getDrawable());
+
+        List<NativeAd.Image> images = nativeAppInstallAd.getImages();
+
+        if (images.size() > 0) {
+            ((ImageView) adView.getImageView()).setImageDrawable(images.get(0).getDrawable());
+        }
+
+        // Some aren't guaranteed, however, and should be checked.
+        if (nativeAppInstallAd.getPrice() == null) {
+            adView.getPriceView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getPriceView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getPriceView()).setText(nativeAppInstallAd.getPrice());
+        }
+
+        if (nativeAppInstallAd.getStore() == null) {
+            adView.getStoreView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getStoreView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getStoreView()).setText(nativeAppInstallAd.getStore());
+        }
+
+        if (nativeAppInstallAd.getStarRating() == null) {
+            adView.getStarRatingView().setVisibility(View.INVISIBLE);
+        } else {
+            ((RatingBar) adView.getStarRatingView())
+                    .setRating(nativeAppInstallAd.getStarRating().floatValue());
+            adView.getStarRatingView().setVisibility(View.VISIBLE);
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeAppInstallAd);
+    }
+
+    *//**
+     * Populates a {@link NativeContentAdView} object with data from a given
+     * {@link NativeContentAd}.
+     *
+     * @param nativeContentAd the object containing the ad's assets
+     * @param adView          the view to be populated
+     *//*
+    private void populateContentAdView(NativeContentAd nativeContentAd,
+                                       NativeContentAdView adView) {
+        adView.setHeadlineView(adView.findViewById(R.id.contentad_headline));
+        adView.setImageView(adView.findViewById(R.id.contentad_image));
+        adView.setBodyView(adView.findViewById(R.id.contentad_body));
+        adView.setCallToActionView(adView.findViewById(R.id.contentad_call_to_action));
+        adView.setLogoView(adView.findViewById(R.id.contentad_logo));
+        adView.setAdvertiserView(adView.findViewById(R.id.contentad_advertiser));
+
+        // Some assets are guaranteed to be in every NativeContentAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeContentAd.getHeadline());
+        ((TextView) adView.getBodyView()).setText(nativeContentAd.getBody());
+        ((TextView) adView.getCallToActionView()).setText(nativeContentAd.getCallToAction());
+        ((TextView) adView.getAdvertiserView()).setText(nativeContentAd.getAdvertiser());
+
+        List<NativeAd.Image> images = nativeContentAd.getImages();
+
+        if (images.size() > 0) {
+            ((ImageView) adView.getImageView()).setImageDrawable(images.get(0).getDrawable());
+        }
+
+        // Some aren't guaranteed, however, and should be checked.
+        NativeAd.Image logoImage = nativeContentAd.getLogo();
+
+        if (logoImage == null) {
+            adView.getLogoView().setVisibility(View.INVISIBLE);
+        } else {
+            ((ImageView) adView.getLogoView()).setImageDrawable(logoImage.getDrawable());
+            adView.getLogoView().setVisibility(View.VISIBLE);
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeContentAd);
+    }
+
+    *//**
+     * Creates a request for a new native ad based on the boolean parameters and calls the
+     * corresponding "populate" method when one is successfully returned.
+     *
+     * @param requestAppInstallAds indicates whether app install ads should be requested
+     * @param requestContentAds    indicates whether content ads should be requested
+     *//*
+    private void refreshAd(boolean requestAppInstallAds, boolean requestContentAds) {
+        if (!requestAppInstallAds && !requestContentAds) {
+            Toast.makeText(this, "At least one ad format must be checked to request an ad.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        AdLoader.Builder builder = new AdLoader.Builder(this, ADMOB_AD_UNIT_ID);
+
+        if (requestAppInstallAds) {
+            builder.forAppInstallAd(new NativeAppInstallAd.OnAppInstallAdLoadedListener() {
+                @Override
+                public void onAppInstallAdLoaded(NativeAppInstallAd ad) {
+                    FrameLayout frameLayout =
+                            (FrameLayout) findViewById(R.id.fl_adplaceholder);
+                    NativeAppInstallAdView adView = (NativeAppInstallAdView) getLayoutInflater()
+                            .inflate(R.layout.ad_app_install, null);
+                    populateAppInstallAdView(ad, adView);
+                    frameLayout.removeAllViews();
+                    frameLayout.addView(adView);
+                }
+            });
+        }
+
+        if (requestContentAds) {
+            builder.forContentAd(new NativeContentAd.OnContentAdLoadedListener() {
+                @Override
+                public void onContentAdLoaded(NativeContentAd ad) {
+                    FrameLayout frameLayout =
+                            (FrameLayout) findViewById(R.id.fl_adplaceholder);
+                    NativeContentAdView adView = (NativeContentAdView) getLayoutInflater()
+                            .inflate(R.layout.ad_content, null);
+                    populateContentAdView(ad, adView);
+                    frameLayout.removeAllViews();
+                    frameLayout.addView(adView);
+                }
+            });
+        }
+
+        AdLoader adLoader = builder.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Toast.makeText(MainActivity.this, "Failed to load native ad: "
+                        + errorCode, Toast.LENGTH_SHORT).show();
+            }
+        }).build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }*/
+}
